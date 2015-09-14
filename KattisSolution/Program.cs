c34909e80b09a0ff5e7926df8cc555d65f7a26d2
Piotr Karpala <pkarpala@infusion.com>
2015-09-15 01:01:30 +0200

    doesn't work

--- KattisSolution/Program.cs
+++ KattisSolution/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Configuration;
 using System.IO;
+using System.Text;
 using KattisSolution.IO;
 
 namespace KattisSolution
@@ -16,13 +18,59 @@ namespace KattisSolution
             IScanner scanner = new OptimizedPositiveIntReader(stdin);
             // uncomment when you need more advanced reader
             // scanner = new Scanner(stdin);
-            // scanner = new LineReader(stdin);
+            scanner = new LineReader(stdin);
             var writer = new BufferedStdoutWriter(stdout);
 
-            var input = scanner.NextInt();
+            var testCases = scanner.NextInt();
 
-            writer.Write(input*5);
-            writer.Write("\n");
+            for (int i = 0; i < testCases; i++)
+            {
+                var line = scanner.Next();
+                var result = 0;
+                int stringNotCovered = line.Length;
+
+                for (int j = 1; j < line.Length; j++)
+                {
+                    if (line.Substring(0, j).StartsWith(line.Substring(j, Math.Min(line.Length - j, j))))
+                    {
+                        //                        if (result == 0)
+                        //                        {
+                        //                            result = j;
+                        //                            break;
+                        //                        }
+                        if (result == 0)
+                        {
+                            result = j;
+
+                            int k = j;
+                            while (k + result < line.Length && line.Substring(k, result) == line.Substring(0, j))
+                            {
+                                k += result;
+                            }
+                            stringNotCovered = line.Length - k - j;
+                        }
+                        else if (result != 0 && j % result == 0)
+                        {
+                            // ignore - we already handled it
+                        }
+                        else
+                        {
+                            // we already have a solution, just check if it covers more ground
+                            // this one doesn't repeat itself
+                            if (stringNotCovered > 0 && 2 * j > line.Length - stringNotCovered)
+                            {
+                                result = j;
+                                stringNotCovered = 0;
+                                break;
+                            }
+                        }
+                    }
+                }
+
+                writer.Write(result);
+                writer.Write("\n");
+            }
+
             writer.Flush();
         }
     }
